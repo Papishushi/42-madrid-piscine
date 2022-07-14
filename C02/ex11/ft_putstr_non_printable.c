cf51bec44ed609720a5c31c5366585981061122a
Daniel Molinero Lucas <danielmolinero111@cyberdude.com>
2022-07-14 12:12:18 +0200

    To Do ex11 ex12 bugged...

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,18 +6,18 @@
 /*   By: dmoliner < dmoliner@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 02:38:33 by dmoliner          #+#    #+#             */
-/*   Updated: 2022/07/14 03:26:39 by dmoliner         ###   ########.fr       */
+/*   Updated: 2022/07/14 12:11:14 by dmoliner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-//#include <stdio.h>
 
 char	*char_to_hex(char c, char *buffer, unsigned int size)
 {
 	int		remainder;
 	int		i;
 
+	i = 0;
 	while (c != 0)
 	{
 		remainder = c % 16;
@@ -41,25 +41,26 @@ int	is_printable(char c)
 void	ft_putstr_non_printable(char *str)
 {
 	int		i;
-	char	buffer[2];
+	char	buffer[4];
 
 	i = 0;
 	while (*(str + i) != '\0')
 	{
-		if (!is_printable(*(str + i)))
+		if (is_printable(*(str + i)) == 0)
 		{
-			write(1, '\\', 1);
-			write(1, char_to_hex(*(str + i), buffer, 2), 2);
+			write(1, "\\", 1);
+			char_to_hex(*(str + i), buffer, 2);
+			write(1, buffer, 2);
 		}
 		else
-			write(1, *(str + i), 1;)
+			write(1, str + i, 1);
+		i++;
 	}
 }
 
-/*
 int	main( void )
 {
 	char	input[] = "Coucou\ntu vas bien ?";
-	printf("Non printable on hex: %s", ft_putstr_non_printable(input));
+	ft_putstr_non_printable(input);
 	return (0);
-}*/
+}
--- C02/ex12/ft_print_memory.c
+++ C02/ex12/ft_print_memory.c
@@ -6,7 +6,7 @@
 /*   By: dmoliner < dmoliner@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 03:07:00 by dmoliner          #+#    #+#             */
-/*   Updated: 2022/07/14 11:37:23 by dmoliner         ###   ########.fr       */
+/*   Updated: 2022/07/14 11:58:32 by dmoliner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,13 +40,13 @@ int	is_printable(char c)
 void	print_hex_contents(void *addr, int og_i)
 {
 	int		i;
-	char	buffer[4];
+	char	buffer[16];
 
 	i = 0;
 	while (i < 16)
 	{
-		write(1, char_to_hex(*(addr + og_i + i), buffer, 4), 4);
-		write(1, ' ', 1);
+		write(1, char_to_hex(*((char	*)(addr + og_i + i)), buffer, 4), 4);
+		write(1, " ", 1);
 		i++;
 	}
 }
@@ -54,31 +54,38 @@ void	print_hex_contents(void *addr, int og_i)
 void	print_contents(void *addr, int og_i)
 {
 	int		i;
-	char	buffer[4];
 
 	i = 0;
 	while (i < 16)
 	{
-		if (is_printable(*(addr + og_i + i)))
-			write(1, *(addr + og_i + i), 1);
+		if (is_printable(*((char	*)(addr + og_i + i))))
+			write(1, addr + og_i + i, 1);
 		else
-			write(1, '.', 1);
+			write(1, ".", 1);
 		i++;
 	}
 }
 
 void	*ft_print_memory(void *addr, unsigned int size)
 {
-	int		i;
+	unsigned int		i;
 
 	i = 0;
 	while (i < size)
 	{
-		write(1, (addr + i), 16);
-		write(1, ': ', 2);
+		write(1, addr + i, 16);
+		write(1, ": ", 2);
 		print_hex_contents(addr, i);
 		print_contents(addr, i);
+		write(1, "\n", 1);
 		i += 16;
 	}
 	return (addr);
+}
+
+int	main( void )
+{
+	char	input[39] = "Hello	world! My name is Daniel	lol		lol";
+	ft_print_memory(input, 39);
+	return (0);
 }
